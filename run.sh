52baa7c7e33e1662dbf20ef3a8426d7bdd9a1a76
Yang Yihan <87106800+yihan1998@users.noreply.github.com>
2021-12-02 01:36:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,19 +8,19 @@ then
     echo "${args}"
 fi
 
-# case $1 in
-#     "tcp")
-#         ./NPtcp -p 0 -C 0 -l 1 -u 8192 -b 8192 -P 80 "${arg}"
-#         ;;
-#     "mtcp")
-#         ./NPmtcp -p 0 -C 0 -l 1 -u 9000 -P 80 "${arg}"
-#         ;;
-#     "cygnus")
-#         ./LD_LIBRARY_PATH=/home/yihan/cygnus/Cygnus:/home/yihan/cygnus/mthread:/home/yihan/Hoard \
-#         ./NPcygnus -p 0 -C 0 -l 1 -u 8192 -P 80 "${arg}"
-#         ;;
-#     *)
-#         echo "Wrong test!"
-#         ;;
-# esac
-# wait
+case $1 in
+    "tcp")
+        ./NPtcp -p 0 -C 0 -l 1 -u 8192 -b 8192 -P 80 "${arg}"
+        ;;
+    "mtcp")
+        ./NPmtcp -p 0 -C 0 -l 1 -u 9000 -P 80 "${arg}"
+        ;;
+    "cygnus")
+        ./LD_LIBRARY_PATH=/home/yihan/cygnus/Cygnus:/home/yihan/cygnus/mthread:/home/yihan/Hoard \
+        ./NPcygnus -p 0 -C 0 -l 1 -u 8192 -P 80 "${arg}"
+        ;;
+    *)
+        echo "Wrong test!"
+        ;;
+esac
+wait
