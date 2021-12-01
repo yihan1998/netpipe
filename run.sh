e709119a63392d0e52b2c05368a84cc5330095a2
Yang Yihan <87106800+yihan1998@users.noreply.github.com>
2021-12-02 01:35:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-arg=$2
+role=$2
 
-case $1 in
-    "tcp")
-        ./NPtcp -p 0 -C 0 -l 1 -u 8192 -b 8192 -P 80 "${arg}"
-        ;;
-    "mtcp")
-        ./NPmtcp -p 0 -C 0 -l 1 -u 9000 -P 80 "${arg}"
-        ;;
-    "cygnus")
-        ./LD_LIBRARY_PATH=/home/yihan/cygnus/Cygnus:/home/yihan/cygnus/mthread:/home/yihan/Hoard \
-        ./NPcygnus -p 0 -C 0 -l 1 -u 8192 -P 80 "${arg}"
-        ;;
-    *)
-        echo "Wrong test!"
-        ;;
-esac
-wait
+if [ $role == 'client' ]
+then
+    args="-h 10.0.0.1 -o $1.txt"
+    echo args
+fi
+
+# case $1 in
+#     "tcp")
+#         ./NPtcp -p 0 -C 0 -l 1 -u 8192 -b 8192 -P 80 "${arg}"
+#         ;;
+#     "mtcp")
+#         ./NPmtcp -p 0 -C 0 -l 1 -u 9000 -P 80 "${arg}"
+#         ;;
+#     "cygnus")
+#         ./LD_LIBRARY_PATH=/home/yihan/cygnus/Cygnus:/home/yihan/cygnus/mthread:/home/yihan/Hoard \
+#         ./NPcygnus -p 0 -C 0 -l 1 -u 8192 -P 80 "${arg}"
+#         ;;
+#     *)
+#         echo "Wrong test!"
+#         ;;
+# esac
+# wait
